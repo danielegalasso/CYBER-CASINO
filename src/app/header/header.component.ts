074f62f9f2eef2e33282d809f1b411d10cd24301
Daniele Galasso <glsdlp03b02h224o@studenti.unical.it>
2023-12-31 09:40:31 +0100

    Routing Perfezionato

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  constructor(private router: Router) {
+    // Aggiungi un ascoltatore per gli eventi di navigazione
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      // Controlla l'URL e imposta selectedNavItem in base alla condizione
+      if (event.url === '/bonus' && this.selectedNavItem !== 'Bonus') {
+        this.selectedNavItem = 'Bonus';
+      }
+    });
+  }
 
   selectedNavItem: string = 'Games'; // Inizializza con il valore predefinito
 
